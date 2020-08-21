e28509b12454bbf90547adcb8a3546c87e9508c0
AAnzel <44969003+AAnzel@users.noreply.github.com>
2020-08-21 11:33:34 +0200

    Fixing active hint script after version change. Script is now more robust as it uses seperate .sed file.

--- pop-shell-ext-change.sh
+++ pop-shell-ext-change.sh
@@ -4,9 +4,25 @@
 
 # Defining sed arguments
 SEDARG_1=-i
+SEDARG_2=-f
+
+# For dark red one, change grey to red in file name
+SEDCOLORSCHEME="Dark_grey_shell.sed"
+
+# Changing hex colors in various files using sed
 
 echo "####################################################################################"
 echo "============ Changing pop-shell active hint color ============"
-POPSHELLFILE="/usr/share/gnome-shell/extensions/pop-shell@system76.com/dark.css"
+echo
+echo "============ Modifying dark.css ============"
+echo
+POPSHELLFILE_1="/usr/share/gnome-shell/extensions/pop-shell@system76.com/dark.css"
 
-sudo sed $SEDARG_1 's/251, 184, 108/249, 112, 107/' $POPSHELLFILE
+sudo sed $SEDARG_1 $SEDARG_2 $SEDCOLORSCHEME $POPSHELLFILE_1
+
+echo "============ Modifying light.css ============"
+echo
+POPSHELLFILE_2="/usr/share/gnome-shell/extensions/pop-shell@system76.com/light.css"
+
+sudo sed $SEDARG_1 $SEDARG_2 $SEDCOLORSCHEME $POPSHELLFILE_2
+
